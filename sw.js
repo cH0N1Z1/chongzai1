d3dbf736c3708a63986c4bf87fe0523bb6ba2307
cH0N1Z1 <15016384258@163.com>
2026-09-12 18:59:10 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='douro-v87';
+const C='douro-v88';
 const ASSETS=['./','./index.html','./style.css','./rogue.css','./sound.js','./shared.js','./rpg.js','./rogue.js','./manifest.json','./icon-192.png','./icon-512.png','./media.json'];
 
 self.addEventListener('install', e => {
@@ -7,4 +7,27 @@ self.addEventListener('install', e => {
   );
 });
 
-self.addEventListener('activate', e
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== C).map(k => caches.delete(k))
+    )).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if(e.request.method !== 'GET' || !e.request.url.startsWith(self.location.origin)){
+    return;
+  }
+  e.respondWith(
+    fetch(e.request).then(r => {
+      if(r && r.status === 200){
+        const clone = r.clone();
+        caches.open(C).then(c => c.put(e.request, clone)).catch(()=>{});
+      }
+      return r;
+    }).catch(() => {
+      return caches.match(e.request).then(r => r || caches.match('./index.html'));
+    })
+  );
+});
